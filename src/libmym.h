45edb0bed99b5cb902e7ddbbef3bf28b862fa17e
Christoph Junghans <junghans@lanl.gov>
2017-03-30 15:03:47 -0600

    added Doxygen

--- src/libmym.h
+++ src/libmym.h
@@ -1,8 +1,10 @@
 #ifndef LIBMYM_H
 #define LIBMYM_H
+
+/** \brief a new tangent function
+ * \param[in] x value to calculate tangent for
+ * \return tangent of x
+ */
 double newtan(double x);
-double newsin(double x);
-double newcos(double x);
 
 #endif
-
